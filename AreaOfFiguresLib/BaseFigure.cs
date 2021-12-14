208b6b342d369dc0893c35e1fd959b296a68b97b
DESKTOP-4NCVT6A\usm44 <sahbeyev@gmail.com>
2021-12-15 00:39:29 +0300

    Library for calculating area of circles and squares.

---
+++ AreaOfFiguresLib/BaseFigure.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace AreaOfFiguresLib
+{
+    public abstract class BaseFigure
+    {
+        public double Result { get; set; }
+        public abstract double Calculate();
+    }
+}
---
+++ AreaOfFiguresLib/Circle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AreaOfFiguresLib
+{
+    public class Circle : BaseFigure
+    {
+        public double Radius { get; set; }
+
+        public Circle(double radius)
+        {
+            if (radius <= 0)
+                throw new ArgumentException("Radius must be more than zero");
+            Radius = radius;
+        }
+
+        override public double Calculate()
+        {
+            return Result = 2 * 3.14f * Radius;
+        }
+    }
+}
